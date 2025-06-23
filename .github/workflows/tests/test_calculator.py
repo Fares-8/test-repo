57a29f5c81691acd1ec00a907f9cdd7f382a2e0d
Fares-8 <F.Alaa2287@nu.edu.eg>
2025-06-23 20:16:58 +0300

    test_calculator.py

--- .github/workflows/tests/test_calculator.py
+++ .github/workflows/tests/test_calculator.py
@@ -1,7 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from calculator import add, subtract
 
 def test_add():
-    assert add(3, 5) == 8
+    assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(10, 4) == 6
